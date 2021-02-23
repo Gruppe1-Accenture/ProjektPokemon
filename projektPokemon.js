3070e4011732d8a355d0f7ab50f6f1c73fbb3568
Klaus Bisanz <klaus.bisanz@accenture.com>
2021-02-23 16:28:28 +0100

    Layout fertig

--- projektPokemon.js
+++ projektPokemon.js
@@ -5,6 +5,9 @@ let name = document.getElementById("namePokemon")
 document.getElementById("button").onclick = myFunction;
 
 async function myFunction() {
+    let x = document.getElementById("gesuchtesPokemon");
+    x.className = "Pokemon";
+    
     index = document.getElementById("input").value;
     pokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + index + "/");
     pokemon = await pokemon.json();
