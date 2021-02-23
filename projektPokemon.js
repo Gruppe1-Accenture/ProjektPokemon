8ffaabafb57d4e7322b1b7c98d45f88fe05751b7
Klaus Bisanz <klaus.bisanz@accenture.com>
2021-02-23 15:10:49 +0100

    Script fertig

--- projektPokemon.js
+++ projektPokemon.js
@@ -1,5 +1,7 @@
 let index = null;
 let pokemon = null;
+let bild = document.getElementById("gesPokemon")
+let name = document.getElementById("namePokemon")
 document.getElementById("button").onclick = myFunction;
 
 async function myFunction() {
@@ -7,4 +9,7 @@ async function myFunction() {
     pokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + index + "/");
     pokemon = await pokemon.json();
     console.log(pokemon);
+
+    bild.src = pokemon.sprites.front_default
+    name.innerHTML = pokemon.name
 };
